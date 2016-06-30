43f7aaf13600c9512b8ed25bc92cbfc2fade2dfd
korkru <kruczek.kornelia@gmail.com>
2016-06-30 06:46:55 +0200

    checkbox

--- js/app.js
+++ js/app.js
@@ -17,4 +17,38 @@ for (i = 0; i < mainElements.length; i++) {
 
 });
 }
+
+var img1=document.querySelector("#photoFirst");
+var img2=document.querySelector("#photoSecond");
+
+img1.addEventListener("mouseover", function(){
+  var divT1=document.querySelector("#divT1");
+  divT1.style.visibility = "hidden";
+  img1.addEventListener("mouseout", function(){
+    var divT1=document.querySelector("#divT1");
+    divT1.style.visibility = "visible";
+
+    img2.addEventListener("mouseover", function(){
+      var divT2=document.querySelector("#divT2");
+        divT2.style.visibility = "hidden";
+        img2.addEventListener("mouseout", function(){
+          var divT2=document.querySelector("#divT2");
+            divT2.style.visibility = "visible";
+    });
+  });
+});
+});
+
+var arrow=querySelectorAll(".arrow");
+for (var i=0; i<img.length; i++);
+arrow[i].addEventListener("click",function(){
+  var imgs=querySelectorAll("#leftSide img");
+  for (var j=0; j<imgs.length; j++);
+  var currentImg=imgs[j];
+  var length=imgs.length;
+
+});
+
+
+
 });
